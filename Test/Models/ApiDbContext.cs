2f302676be8b2443f5fa57a7e4e441aa33d5c089
Nadeen Hassan <ibrahim_nadeen@yahoo.com>
2025-07-15 10:59:10 +0300

    Add project files.

---
+++ Test/Models/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace Test.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions option) : base(option)
+        {
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+
+}
